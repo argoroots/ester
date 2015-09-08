6119575ef0ea718b642502f99a41574a2aff0d37
Argo Roots <argo@roots.ee>
2015-09-08 20:46:32 +0300

    test 2

--- worker.js
+++ worker.js
@@ -138,7 +138,10 @@ function humanJson(marc) {
         }
     }
     for(a1 in op.get(marc, [700, 'fields'], [])) { //authors
-        op.push(tags, op.get(authormapping, op.get(marc, [700, 'fields', a1, 'e', 0], ''), op.get(marc, [700, 'fields', a1, 'e', 0])), op.get(marc, [700, 'fields', a1, 'a', 0]))
+        var autor_tag = op.get(marc, [700, 'fields', a1, 'e', 0])
+        var autor_value = op.get(marc, [700, 'fields', a1, 'a', 0])
+        if(op.has(authormapping, autor_tag)) autor_tag = op.get(authormapping, autor_tag)
+        if(autor_tag && autor_value) op.push(tags, autor_tag, autor_value)
     }
     console.log(JSON.stringify(tags, null, '   '))
     for(k in tags) {
