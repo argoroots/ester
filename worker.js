6177215c45d396dcbf35ae6035d503221973d0bf
Argo Roots <argo@roots.ee>
2015-09-09 00:01:02 +0300

    Get one item by ID

--- worker.js
+++ worker.js
@@ -235,9 +235,28 @@ express()
     })
 
     .use('/item/:id', function(req, res, next) {
+        var format = req.query.f
+        if(['human', 'simple', 'concat', 'json', 'marc'].indexOf(format) === -1) format = 'human'
+
         var id = req.params.q
-        var format = req.query.f || 'marc'
-        var results = []
+        if(!id) return next(new Error('No ID!'))
+
+        var filename = path.join(APP_TMPDIR, id + '.json')
+        if(!fs.existsSync(filename)) return next(new Error('Invalid ID!'))
+
+        var full_result = require(filename)
+
+        if(format === 'marc') {
+            res.set('Content-Type', 'text/plain; charset=utf-8')
+            res.send(full_result.marc)
+        } else {
+            res.send({
+                result: full_result[format],
+                version: APP_VERSION,
+                started: APP_STARTED
+            })
+        }
+
     })
 
 
