48343a9fd844868ac95d6979e88dc689b75b5b1d
Argo Roots <argo@roots.ee>
2015-09-27 14:40:59 +0300

    Sentry fix

--- worker.js
+++ worker.js
@@ -9,7 +9,7 @@ var raven   = require('raven')
 
 
 // global variables (and list of all used environment variables)
-APP_VERSION = require('./package').version
+APP_VERSION = process.env.VERSION || require('./package').version
 APP_STARTED = new Date().toISOString()
 APP_PORT    = process.env.PORT || 3000
 APP_TMPDIR  = process.env.TMPDIR || path.join(__dirname, 'tmp')
@@ -21,19 +21,31 @@ fs.existsSync(APP_TMPDIR) || fs.mkdirSync(APP_TMPDIR)
 
 
 
+// initialize getsentry.com client
+var raven_client = new raven.Client({
+    release: APP_VERSION,
+    dataCallback: function(data) {
+        delete data.request.env
+        return data
+    }
+})
+
+
+
+// start express app
 express()
     // set CORS
     .use(cors())
 
     // logs to getsentry.com - start
-    .use(raven.middleware.express.requestHandler(APP_SENTRY))
+    .use(raven.middleware.express.requestHandler(raven_client))
 
     // routes mapping
     .use('/search', require('./routes/search'))
     .use('/item', require('./routes/item'))
 
     // logs to getsentry.com - error
-    .use(raven.middleware.express.errorHandler(APP_SENTRY))
+    .use(raven.middleware.express.errorHandler(raven_client))
 
     // show error
     .use(function(err, req, res, next) {
