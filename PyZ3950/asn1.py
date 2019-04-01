3a14483ca96ae8022ac35d921b131cd89483692e
Argo Roots <argo@roots.ee>
2019-04-02 00:44:29 +0300

    Fix asn1 utf8

--- PyZ3950/asn1.py
+++ PyZ3950/asn1.py
@@ -357,7 +357,8 @@ class CtxBase:
     def get_codec (self, base_tag):
         def default_enc (x):
             if isinstance (x, str):
-                return (x.encode ('ascii'), 0)
+                # return (x.encode ('ascii'), 0)
+                return (x.encode ('utf-8'), 0)
             elif isinstance (x, basestring):
                 return (x.encode ('utf-8'), 0)
             return (x, 0)
