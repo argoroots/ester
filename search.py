23c3544b195311f4bafd33a4e1b3805447c9c6d6
Argo Roots <argo@roots.ee>
2019-04-02 14:05:49 +0300

    Add CORS

--- search.py
+++ search.py
@@ -109,5 +109,8 @@ def handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        },
         'body': json.dumps(result)
     }
